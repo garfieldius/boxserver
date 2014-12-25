ffb2ceda32d36582cb0b7c0063a4d11b87f51e3e
Georg Großberger <contact@grossberger-ge.org>
2014-12-25 13:11:26 +0100

    Validate a version string

--- util/validate.go
+++ util/validate.go
@@ -5,11 +5,17 @@ import (
 )
 
 var validIdentifier *regexp.Regexp
+var validVersion *regexp.Regexp
 
 func init() {
 	validIdentifier = regexp.MustCompile(`^[a-z0-9][a-z0-9_\-\.]+[a-z0-9]$`)
+	validVersion    = regexp.MustCompile(`^[0-9]+\.[0-9]+\.[0-9]+$`)
 }
 
 func ValidKey(key string) bool {
 	return validIdentifier.Match(([]byte)(key))
+}
+
+func ValidVersion(version string) bool {
+	return validVersion.Match(([]byte)(version))
 }
